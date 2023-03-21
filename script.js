aca28c44fa9c51c3a19d4c416d4e197bdf3ce36b
mannbajpai <mannbajpai.grs@gmail.com>
2023-03-21 22:22:05 +0530

    scriptjs corrected

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 // Selecting DOM elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
